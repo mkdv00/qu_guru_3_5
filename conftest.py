d2a910a10f75986f1339a16f192a53c67d984c8a
Maksim <kudaev.m@nakukop.com>
2022-12-06 00:23:11 +0300

    Fixed problems

--- conftest.py
+++ conftest.py
@@ -1,16 +1,11 @@
 import pytest
-from selene import have, command
 from selene.support.shared import browser
 
 
 @pytest.fixture
 def open_browser():
-    browser.config.timeout = 3.0
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.open('https://demoqa.com/automation-practice-form')
-    ads = browser.all('[id^=google_ads_][id$=container__]')
-    ads.should(have.size_less_than_or_equal(3))
-    ads.perform(command.js.remove)
     yield
     browser.quit()
--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,9 +1,12 @@
-from selene.support.shared import browser
-from selene import be, have
 import os
+
+from selene import be, have, command
+from selene.support.shared import browser
 
 
 def test_fill_form(open_browser):
+    browser.open("/automation-practice-form")
+
     browser.element("#firstName").should(be.blank).type("Maks")
     browser.element("#lastName").should(be.blank).type("Kudaev")
     browser.element("#userEmail").should(be.blank).type("maxim.cudaew@gmail.com")
@@ -15,11 +18,16 @@ def test_fill_form(open_browser):
     browser.element('.react-datepicker__day--022').click()
     browser.element("#subjectsInput").type("Computer Science").press_enter()
 
+    ads = browser.all('[id^=google_ads_][id$=container__]')
+    ads.should(have.size_less_than_or_equal(3))
+    ads.perform(command.js.remove)
+
     checkboxes = browser.elements(".custom-checkbox label")
     for checkbox in checkboxes:
         checkbox.click()
 
-    browser.element("#uploadPicture").send_keys(os.path.abspath("../files/photo.jpg"))
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'files/photo.jpg')))
     browser.element("#currentAddress").should(be.blank).type("It's my address")
     browser.element("#react-select-3-input").type("NCR").press_enter()
     browser.element("#react-select-4-input").type("Delhi").press_enter()
@@ -37,4 +45,3 @@ def test_fill_form(open_browser):
         "It's my address",
         "NCR Delhi"
     ))
-
